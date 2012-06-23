3a48c048de07c4d26da094320cc91a0073396bac
Peter Hsu <findlimit@gmail.com>
2012-06-23 11:15:48 +0800

    Merge files into web/

--- openhci2011/js/program.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	newValue = $(".eventDay td:last-child").attr("rowspan")*2;
-	$(".eventDay td:last-child").attr("rowspan", newValue);
-	
-	$(".program td:first-child").width($(".program").width()*0.15);
-	$(".program td:nth-child(2)").width($(".program").width()*0.75);
-	$(".program td:nth-child(3)").width($(".program").width()*0.1);
-});
